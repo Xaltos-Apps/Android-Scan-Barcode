21fbdb83c16496853eb2a034c55d8d50f73916b9
tiroman <master_t@inbox.ru>
2017-12-01 09:44:17 +0300

    sdk version upgrade

--- scanbarcode/src/main/java/com/tiromansev/scanbarcode/CaptureActivity.java
+++ scanbarcode/src/main/java/com/tiromansev/scanbarcode/CaptureActivity.java
@@ -82,6 +82,7 @@ public class CaptureActivity extends AppCompatActivity implements SurfaceHolder.
 
         Window window = getWindow();
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_capture);
 
         hasSurface = false;
         inactivityTimer = new InactivityTimer(this);
