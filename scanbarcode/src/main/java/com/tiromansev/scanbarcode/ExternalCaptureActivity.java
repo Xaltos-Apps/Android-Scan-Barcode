796f9a82d246961beb121bee901391d62129ef5c
ronin <tiromansev@gmail.com>
2018-04-15 22:55:05 +0300

    layout improvements

--- scanbarcode/src/main/java/com/tiromansev/scanbarcode/ExternalCaptureActivity.java
+++ scanbarcode/src/main/java/com/tiromansev/scanbarcode/ExternalCaptureActivity.java
@@ -1,6 +1,7 @@
 package com.tiromansev.scanbarcode;
 
 import android.content.SharedPreferences;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
@@ -22,6 +23,7 @@ public class ExternalCaptureActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_external_capture);
         edtBarcode = findViewById(R.id.edtBarcode);
+        edtBarcode.getBackground().mutate().setColorFilter(getResources().getColor(R.color.color_external_caption), PorterDuff.Mode.SRC_ATOP);
 
         edtBarcode.setOnKeyListener(new View.OnKeyListener() {
             @Override
