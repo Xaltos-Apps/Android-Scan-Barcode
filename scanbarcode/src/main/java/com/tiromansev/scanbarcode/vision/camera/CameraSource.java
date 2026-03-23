bf1853b094920ad02e8bd9babbd267fec84b437f
Oxana Kostikova <lereenadem@yandex.ru>
2026-03-23 23:09:50 +0100

    Migrate to Google ML kit

--- scanbarcode/src/main/java/com/tiromansev/scanbarcode/vision/camera/CameraSource.java
+++ scanbarcode/src/main/java/com/tiromansev/scanbarcode/vision/camera/CameraSource.java
@@ -5,22 +5,17 @@ import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.hardware.Camera.CameraInfo;
 import android.hardware.Camera.Parameters;
-import android.util.Log;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.WindowManager;
-
-import com.google.android.gms.common.images.Size;
-import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
-import com.tiromansev.scanbarcode.R;
 import com.tiromansev.scanbarcode.vision.PreferenceUtils;
 import com.tiromansev.scanbarcode.vision.Utils;
-
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.IdentityHashMap;
 import java.util.List;
 import java.util.Map;
+
+
 
 /**
  * Manages the camera and allows UI updates on top of it (e.g. overlaying extra Graphics). This
@@ -45,7 +40,7 @@ public class CameraSource {
   private static final float REQUESTED_CAMERA_FPS = 30.0f;
 
   private Camera camera;
-  @FirebaseVisionImageMetadata.Rotation private int rotation;
+  private int rotation;
 
   private Size previewSize;
 
